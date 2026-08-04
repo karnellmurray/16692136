cae2196e84cd4137ac4df2409c55ecbc6c3e20c6
unknown <karnellmurray@hotmail.com>
2026-08-04 13:07:54 +0100

    Update production API_URL to new Render service URL

--- Frontend/js/config.js
+++ Frontend/js/config.js
@@ -2,4 +2,4 @@
 
 const API_URL = ['localhost', '127.0.0.1'].includes(window.location.hostname)
   ? 'http://localhost:3000'
-  : 'https://one6692136.onrender.com'
+  : 'https://one6692136-1.onrender.com'
